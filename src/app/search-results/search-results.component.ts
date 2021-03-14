5f68892ee03c824bdbbad42092fad7c395a71bce
manoj <gantamannoj@gmail.com>
2021-03-14 11:50:53 -0400

    Added dummy service file

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Show} from '../models/show.model';
+import {DummyService} from '../services/dummy.service';
 
 @Component({
   selector: 'app-search-results',
@@ -8,34 +9,10 @@ import {Show} from '../models/show.model';
 })
 export class SearchResultsComponent implements OnInit {
   shows: Array<Show>;
-  constructor() {
-    this.shows = [new Show({
-      name: 'Doctor Who',
-      language: 'English',
-      genres: ['Sci-Fi', 'Action', 'Adventure'],
-      id: 210,
-      summary: 'A show about a time travelling alien called The Doctor.',
-      status: 'ongoing',
-      image: 'https://static.tvmaze.com/uploads/images/medium_portrait/231/579166.jpg'
-    }),
-      new Show({
-        name: 'The Magicians',
-        language: 'English',
-        genres: ['Sci-Fi', 'Action', 'Adventure'],
-        id: 210,
-        summary: 'A show about magicians and their trauma.',
-        status: 'ongoing',
-        image: 'https://static.tvmaze.com/uploads/images/medium_portrait/231/579166.jpg'
-      }),
-      new Show({
-        name: 'Smallville',
-        language: 'English',
-        genres: ['Sci-Fi', 'Action', 'Adventure'],
-        id: 210,
-        summary: 'A show about Superman as a teenager.',
-        status: 'ongoing',
-        image: 'https://static.tvmaze.com/uploads/images/medium_portrait/231/579166.jpg'
-      })];
+  constructor(private dummyService: DummyService) {
+    this.dummyService.getShows().subscribe((s) => {
+      this.shows = s;
+    });
   }
 
   ngOnInit(): void {
